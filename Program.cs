0d974d62c949ee26460befc8dd447d88a49bc6f8
vinicius_santos <vinisantosr2511@gmail.com>
2024-09-01 14:56:56 -0300

    desafio realizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone1 = new Iphone("meu iphone", "15 Pro Max", "123456789", 128);
+
+iphone1.Ligar();
+iphone1.ReceberLigacao();
+iphone1.InstalarAplicativo("Instagram");
+
+Nokia nokia1 = new Nokia("meu nokia", "tijolão", "987654321", 16);
+
+nokia1.Ligar();
+nokia1.ReceberLigacao();
+nokia1.InstalarAplicativo("Instagram");
